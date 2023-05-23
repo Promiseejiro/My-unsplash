736004de795fe5751049e7e797a9f1c1ae1dd642
Promise Ejiro <promiseejiro43@gmail.com>
2023-05-23 14:28:53 +0100

    Update cloudinarySetup.ts

--- server/src/utils/cloudinarySetup.ts
+++ server/src/utils/cloudinarySetup.ts
@@ -1,8 +1,1 @@
-const cloudinary = require("cloudinary").v2;
-cloudinary.config({
-  cloud_name: "dxqg5hify",
-  api_key: "839919721256388",
-  api_secret: "kqu9QSxT_puF2z2kDtHwSmlckkE"
-});
 
-export default cloudinary;
