d995f50759c0bdd013fa57e24c4bd4c609da79d8
Promiseejiro <promiseejiro43@gmail.com>
2023-05-26 14:18:26 -0700

    new commit

--- client/src/components/photos.tsx
+++ client/src/components/photos.tsx
@@ -1,5 +1,6 @@
 //components
 import Photo from "./photo";
+import Header from "./header";
 //dependencies
 interface PhotosIn {
   photos: any;
@@ -9,6 +10,7 @@ interface PhotosIn {
 const Photos = ({ photos, deletehandler }: PhotosIn) => {
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 md:gap-4 lg:grid-cols-3">
+      {photos.length === 0 && <Header title={"Add photo"} />}
       {photos.map((photo: any) => (
         <Photo
           photoObjects={photo}
